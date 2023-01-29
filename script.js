72752670557acd948fd025f2c88bea068b4b0592
Kunal Pardhi <120072638+kunal-hash@users.noreply.github.com>
2023-01-29 22:41:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	let string = str;
+	let reverse = split("str").reverse().join();
+	if(reverse == string){
+		return true;
+		else{
+		return false;
+		}
+	}
 
 }
 module.exports = palindrome
